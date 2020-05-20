894ac7a261c969f1632b91304b41795cc91cd3f1
Martin Haaß <martin.haass@connyun.com>
2020-05-20 10:08:48 +0200

    add option to replace marked block in a given file (e.g. README), start/end tags are hardcoded

--- openapi2slate.js
+++ openapi2slate.js
@@ -1,10 +1,16 @@
 var _ = require('lodash');
 var RefParser = require('json-schema-ref-parser');
+const util = require('util')
+const fs = require('fs')
 
 var info = require('./info.js');
 var responses = require('./responses.js');
 var definitions = require('./definitions.js');
 var paths = require('./paths.js');
+
+//
+const start_pattern = `Available paths (auto-generated!):\n\`\`\``
+const stop_pattern = `\`\`\``
 
 module.exports = {
   printSlateMarkdown: function(api, program) {
@@ -17,8 +23,11 @@ module.exports = {
         }
 
         if (program.pathsOnly) {
-          let output = printPathsOnly(epPaths);
-          console.log(output)
+          let output = printPathsOnly(epPaths, start_pattern, stop_pattern);
+          if (program.readme) {
+            replaceBlock(program.readme, output, start_pattern, stop_pattern)
+          } else {
+            console.log(output)
           }
         } else 
           printNormal(epPaths, api, program.includeInternal);
@@ -30,25 +39,21 @@ module.exports = {
 };
 
 const methods=['get','post', 'put','delete','options', 'head']
-const printPathsOnly = (epPaths) => {
-  const util = require('util')
-  let parameters ="Available paths:\n```\n"
+
+const printPathsOnly = (epPaths, start_pattern, stop_pattern) => {
+  let parameters = start_pattern + "\n"
   epPaths = epPaths.sort( (a,b) => (a.endpoint > b.endpoint)? 1 : -1 )
-  epPaths.forEach(path => {
+  epPaths.forEach( path => {
     
     methods.forEach( method =>{
-      if (!path.hasOwnProperty(method))
+      if ( !path.hasOwnProperty(method) )
         return
       let type = method.toUpperCase().padEnd(10,' ')
-<<<<<<< HEAD
-      let paramList = path[method].parameters.filter(el => el.in === 'query' && el.required==true).map(el=>`${el.name}=[${el.name}]`).join('&')
-=======
       let paramList = (path[method].parameters)? path[method].parameters.filter( el => el.in === 'query' && el.required==true ).map(el=>`${el.name}=[${el.name}]`).join('&') : ''
->>>>>>> 000429e...  merge with optionpatjs
       parameters += util.format(`${type} ${path['endpoint']}?${paramList}\n`)
     })
   });
-  parameters+='\n```\n'
+  parameters += "\n"+stop_pattern+"\n"
   return parameters
 }
 
@@ -61,4 +66,20 @@ const printNormal= (epPaths, api, includeInternal)=> {
   // Responses + Definitions
   console.log(responses.responsesSection(api.responses, 1));
   console.log(definitions.definitionsObject(api.definitions));
+}
+
+const replaceBlock = (file, output, start_pattern, stop_pattern) => {
+
+  fs.readFile(file, 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  start_pattern = start_pattern.replace("(","\\(").replace(")","\\)")
+  const regex = new RegExp(`${start_pattern}.*?${stop_pattern}`,"s");
+  var result = data.replace(regex, output)
+
+  fs.writeFile(file, result, 'utf8', function (err) {
+     if (err) return console.log(err);
+  });
+});
 }
